b730a58ffb5f9b6e0834f23e235ec0c533714663
김동욱 <ehddnr73@naver.com>
2025-05-19 15:05:57 +0900

    프로젝트 생성

---
+++ spring-ai-mcp-weather-stdio-server/src/main/java/com/yeolmok/springaimcpweatherstdioserver/SpringAiMcpWeatherStdioServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeolmok.springaimcpweatherstdioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpWeatherStdioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMcpWeatherStdioServerApplication.class, args);
+    }
+
+}
---
+++ spring-ai-mcp-weather-stdio-server/src/test/java/com/yeolmok/springaimcpweatherstdioserver/SpringAiMcpWeatherStdioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeolmok.springaimcpweatherstdioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpWeatherStdioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
